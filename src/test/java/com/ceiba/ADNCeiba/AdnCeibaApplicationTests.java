da49f99ff3b05728a789a52dbf623378277efca0
Sergio Bermúdez Tarazona - Ceiba Software <sergio.bermudez@ceiba.com.co>
2020-12-18 09:22:11 -0500

    Estrcutura proyecto gradle

---
+++ src/test/java/com/ceiba/ADNCeiba/AdnCeibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.ADNCeiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdnCeibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
